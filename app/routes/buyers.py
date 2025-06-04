7a5f03abcce637cf6190ec2df38f38b2fd720e12
balu72 <gbalu72@gmail.com>
2025-06-04 16:37:46 +0530

    Added states

--- app/routes/buyers.py
+++ app/routes/buyers.py
@@ -257,13 +257,41 @@ def get_states():
                 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand',
                 'West Bengal'
             ],
-            'USA': ['California', 'New York', 'Florida', 'Texas', 'Nevada'],
-            'UK': ['England', 'Scotland', 'Wales', 'Northern Ireland'],
-            'Germany': ['Bavaria', 'Berlin', 'Hamburg', 'Saxony'],
-            'France': ['Île-de-France', 'Provence', 'Normandy', 'Brittany'],
-            'Australia': ['New South Wales', 'Victoria', 'Queensland', 'Western Australia'],
-            'Canada': ['Ontario', 'Quebec', 'British Columbia', 'Alberta'],
-            'Singapore': ['Central', 'North', 'South', 'East', 'West']
+            'USA': [
+                'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+                'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+                'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+                'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+                'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+                'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+                'Wisconsin', 'Wyoming'
+            ],
+            'UK': [
+                    'England', 'Scotland', 'Wales', 'Northern Ireland'
+                ],
+            'Germany': [
+                'Baden-Württemberg', 'Bavaria', 'Berlin', 'Brandenburg', 'Bremen', 'Hamburg', 'Hesse',
+                'Lower Saxony', 'Mecklenburg-Vorpommern', 'North Rhine-Westphalia', 'Rhineland-Palatinate',
+                'Saarland', 'Saxony', 'Saxony-Anhalt', 'Schleswig-Holstein', 'Thuringia'
+            ],
+            'France': [
+                'Auvergne-Rhône-Alpes', 'Bourgogne-Franche-Comté', 'Brittany', 'Centre-Val de Loire',
+                'Corsica', 'Grand Est', 'Hauts-de-France', 'Île-de-France', 'Normandy', 'Nouvelle-Aquitaine',
+                'Occitanie', 'Pays de la Loire', 'Provence-Alpes-Côte d\'Azur'
+            ],
+            'Australia': [
+                'New South Wales', 'Victoria', 'Queensland', 'Western Australia', 'South Australia',
+                'Tasmania', 'Northern Territory', 'Australian Capital Territory'
+            ],
+            'Canada': [
+                'Alberta', 'British Columbia', 'Manitoba', 'New Brunswick', 'Newfoundland and Labrador',
+                'Nova Scotia', 'Ontario', 'Prince Edward Island', 'Quebec', 'Saskatchewan', 'Northwest Territories',
+                'Nunavut', 'Yukon'
+            ],
+            'Singapore': [
+                'Central Region', 'East Region', 'North Region', 'North-East Region', 'West Region'
+            ]
         }
         state_list = static_data_states.get(country, [])
     
